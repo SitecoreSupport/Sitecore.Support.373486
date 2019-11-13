5c633088710f54a5c8bcf113e8e6931fb8d4b2c7
Nazim Ali <nazim.ali@sitecore.com>
2019-11-13 13:53:08 -0500

    Create patch for bug no 373486

---
+++ src/Sitecore.Support.373486/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.373486")]
+[assembly: AssemblyProduct("Sitecore.Support.373486")]
+[assembly: ComVisible(false)]
